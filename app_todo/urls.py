66e4a30895ba4f03f06878d291f26ab3d7c24bce
Amol Kahat <akahat@redhat.com>
2017-09-17 00:27:55 +0530

    Added completed task.

--- app_todo/urls.py
+++ app_todo/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^delete/[0-9]+', views.delete_db, name='delete'),
+    url(r'^delete/([0-9]+)$', views.delete_db, name='delete'),
     url(r'add', views.add_db, name='add'),
+    url(r'completed', views.completed, name='completed'),
+    url(r'complete/([0-9]+)$', views.complete_task, name='complete'),
         ]
--- app_todo/views.py
+++ app_todo/views.py
@@ -7,7 +7,7 @@ from .models import tasks
 # Create your views here.
 
 def index(request):
-    layouts_tasks = tasks.objects.order_by('added_date')
+    layouts_tasks = tasks.objects.filter(state=-1)
     output = [t for t in layouts_tasks]
     if output is not None:
         context = {'list_tasks' : output}
@@ -25,9 +25,22 @@ def add_db(request):
     new_task.save()
     return HttpResponseRedirect('/todo', {'added': True})
 
-def delete_db(request):
-    import pdb
-    pdb.set_trace()
-    tid = request.POST.get('id')
+def delete_db(request, tid):
     tasks.objects.filter(id=tid).delete()
     return HttpResponseRedirect('/todo', {'deleted': True})
+
+def complete_task(request, tid):
+    data = tasks.objects.get(id=tid)
+    data.state = 0
+    data.save()
+    return HttpResponseRedirect('/todo', {'update': True})
+
+def completed(request):
+    data = tasks.objects.filter(state=0)
+    output = [t for t in data]
+    if output is not None:
+        context = {'list_tasks': output}
+        return render(request, 'completed.html', context)
+    else:
+        context = {'nothing': 'Nothing to print'}
+        return render(request, 'completed.html', context)
